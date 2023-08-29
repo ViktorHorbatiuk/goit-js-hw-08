6be69be2ada5a2843da889d5ca6679038750f6d4
ViktorHorbatiuk <horbatiuk.va@gmail.com>
2023-08-29 15:35:49 +0200

    update task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const KEY_OF_TIME_STORAGE = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe, {
+  loop: true,
+  fullscreen: true,
+  quality: '1080p',
+});
+
+const getCurrentTime = function (currentTime) {
+  const seconds = currentTime.seconds;
+  localStorage.setItem(KEY_OF_TIME_STORAGE, JSON.stringify(seconds));
+};
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem(KEY_OF_TIME_STORAGE)) || 0
+);
