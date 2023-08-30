ff75b1a51142f14a4a9c9ef90a161abab33b1089
ViktorHorbatiuk <horbatiuk.va@gmail.com>
2023-08-30 14:56:05 +0200

    update task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const KEY_OF_LOCALSTORAGE = "feedback-form-state";
+let formData = {};
+
+form.addEventListener("input", throttle(saveMessage, 500));
+
+function saveMessage(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(KEY_OF_LOCALSTORAGE, JSON.stringify(formData));
+}
+
+form.addEventListener('submit', submitFormOn);
+
+function submitFormOn(e) {
+    e.preventDefault();
+    console.log(JSON.parse(localStorage.getItem(KEY_OF_LOCALSTORAGE)));
+    e.currentTarget.reset();
+    localStorage.removeItem(KEY_OF_LOCALSTORAGE);
+}
+
+function updateForm() {
+    let data = localStorage.getItem(KEY_OF_LOCALSTORAGE);
+    if (data) {
+        data = JSON.parse(data);
+        Object.entries(data).forEach(([name, value]) => {
+            formData[name] = value;
+            form.elements[name].value = value;
+        });
+    }
+}
+
+updateForm();
